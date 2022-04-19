274b6174213a35bbca03f9b67fa9ed87cd1c882b
haul <66765822+haull@users.noreply.github.com>
2022-04-20 01:31:41 +0700

    Add files via upload

---
+++ 10119244Latihan1/app/src/main/java/com/example/a10119244latihan1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a10119244latihan1
+//tanggal pengerjaan : 19 April 2022
+//nim                : 10119244
+//nama               : Mya Hauliyah
+//kelas              : IF6
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
